d851527613e9bf0e8cca665b76583862ca305e05
Heather Peterson <hpeterson462@gmail.com>
2020-10-31 15:17:02 -0700

    seed data

---
+++ data/seed.js
@@ -0,0 +1,21 @@
+const UserService = require('../lib/services/user-service');
+const Route = require('../lib/models/route');
+
+module.exports = async () => {
+  await Promise.all([...Array(user)].map((_, i) => {
+    return UserService.create({
+      email: `test${i + 1}@test.com`,
+      password: `password${i + 1}`
+    });
+  }));
+
+  await Promise.all([...Array(route)].map((_, i) => {
+    return Route.insert({
+      userId: `${Math.floor(i / 2) + 1}`,
+      location: 'Smith Rock',
+      name: 'Voyage of the Cowdog',
+      rating: '5.8+',
+      notes: 'It begins...',
+    });
+  }));
+};
